5db928a84f6c38ef80613de2a80a65236f7e472e
LEUBE <100429009+LEUBE@users.noreply.github.com>
2024-01-01 21:06:24 +0100

    Change to Sumry Inverter
    GUI Tiles were not defined, deemed to be taken from standard inverter device

--- install/install.sh
+++ install/install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-read -p "Install Chargery BMS on Venus OS at your own risk? [Y to proceed]" -n 1 -r
+read -p "Install Sumry Inverter on Venus OS at your own risk? [Y to proceed]" -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	echo "Download driver and library"
 
-	wget https://github.com/Tobi177/venus-chargerybms/archive/master.zip
+	wget https://github.com/LEUBE/venus-sumryinverter/archive/master.zip
 	unzip master.zip
 	rm master.zip
 
@@ -14,43 +14,43 @@ then
 	unzip master.zip
 	rm master.zip
 
-	mkdir -p venus-chargerybms-master/ext/velib_python
-	cp -R velib_python-master/* venus-chargerybms-master/ext/velib_python
+	mkdir -p venus-sumryinverter-master/ext/velib_python
+	cp -R velib_python-master/* venus-sumryinverter-master/ext/velib_python
 
-	echo "Add Chargery entries to serial-starter"
-	sed -i  's/ENV{VE_SERVICE}="rs485:default"/ENV{VE_SERVICE}="rs485:default:chargerybms"/g' /etc/udev/rules.d/serial-starter.rules
-	sed -i  '/service.*imt.*dbus-imt-si-rs485tc/a service chargerybms     chargerybms' /etc/venus/serial-starter.conf
+	echo "Add Sumry entries to serial-starter"
+	sed -i  's/ENV{VE_SERVICE}="rs485:default"/ENV{VE_SERVICE}="rs485:default:sumryinverter"/g' /etc/udev/rules.d/serial-starter.rules
+	sed -i  '/service.*imt.*dbus-imt-si-rs485tc/a service sumryinverter     sumryinverter' /etc/venus/serial-starter.conf
 
-	echo "Install Chargery driver"
-	mkdir -p /var/log/chargerybms
-	mkdir -p /opt/victronenergy/chargerybms
-	cp -R venus-chargerybms-master/ext /opt/victronenergy/chargerybms
-	cp -R venus-chargerybms-master/driver/* /opt/victronenergy/chargerybms
+	echo "Install Sumry driver"
+	mkdir -p /var/log/sumryinverter
+	mkdir -p /opt/victronenergy/sumryinverter
+	cp -R venus-sumryinverter-master/ext /opt/victronenergy/sumryinverter
+	cp -R venus-sumryinverter-master/driver/* /opt/victronenergy/sumryinverter
 
-	chmod +x /opt/victronenergy/chargerybms/start-chargerybms.sh
-	chmod +x /opt/victronenergy/chargerybms/chargerybms.py
-	chmod +x /opt/victronenergy/chargerybms/service/run
-	chmod +x /opt/victronenergy/chargerybms/service/log/run
+	chmod +x /opt/victronenergy/sumryinverter/start-sumryinverter.sh
+	chmod +x /opt/victronenergy/sumryinverter/sumryinverter.py
+	chmod +x /opt/victronenergy/sumryinverter/service/run
+	chmod +x /opt/victronenergy/sumryinverter/service/log/run
 
-	ln -s /opt/victronenergy/chargerybms/service /opt/victronenergy/service/chargerybms
-	ln -s /opt/victronenergy/chargerybms/service /opt/victronenergy/service-templates/chargerybms
+	ln -s /opt/victronenergy/sumryinverter/service /opt/victronenergy/service/sumryinverter
+	ln -s /opt/victronenergy/sumryinverter/service /opt/victronenergy/service-templates/sumryinverter
 
-	echo "Copy gui files"
+	#echo "Copy gui files" This should not be necessary as the inverter will use the standard GUI
 
-	cp venus-chargerybms-master/gui/qml/MbItemRowTOBO.qml /opt/victronenergy/gui/qml
-	cp venus-chargerybms-master/gui/qml/MbTextDescriptionTOBO.qml /opt/victronenergy/gui/qml
-	cp venus-chargerybms-master/gui/qml/PageBatteryChargeryBMS.qml /opt/victronenergy/gui/qml
-	cp venus-chargerybms-master/gui/qml/PageBatteryChargeryBMSImpedances.qml /opt/victronenergy/gui/qml
-	cp venus-chargerybms-master/gui/qml/PageBatteryChargeryBMSVoltages.qml /opt/victronenergy/gui/qml
-	cp venus-chargerybms-master/gui/qml/PageMain.qml /opt/victronenergy/gui/qml
+	#cp venus-sumryinverter-master/gui/qml/MbItemRowTOBO.qml /opt/victronenergy/gui/qml
+	#cp venus-sumryinverter-master/gui/qml/MbTextDescriptionTOBO.qml /opt/victronenergy/gui/qml
+	#cp venus-sumryinverter-master/gui/qml/PageBatteryChargeryBMS.qml /opt/victronenergy/gui/qml
+	#cp venus-sumryinverter-master/gui/qml/PageBatteryChargeryBMSImpedances.qml /opt/victronenergy/gui/qml
+	#cp venus-sumryinverter-master/gui/qml/PageBatteryChargeryBMSVoltages.qml /opt/victronenergy/gui/qml
+	#cp venus-sumryinverter-master/gui/qml/PageMain.qml /opt/victronenergy/gui/qml
 
-	read -p "Setup new gui overview? [Y to proceed]" -n 1 -r
-	echo    # (optional) move to a new line
-	if [[ $REPLY =~ ^[Yy]$ ]]
-	then
-		echo "Setup new overview"
-		cp venus-chargerybms-master/gui/qml/OverviewTiles.qml /opt/victronenergy/gui/qml
-	fi
+	#read -p "Setup new gui overview? [Y to proceed]" -n 1 -r
+	#echo    # (optional) move to a new line
+	#if [[ $REPLY =~ ^[Yy]$ ]]
+	#then
+	#	echo "Setup new overview"
+	#	cp venus-chargerybms-master/gui/qml/OverviewTiles.qml /opt/victronenergy/gui/qml
+	#fi
 
 	echo "To finish, reboot the Venus OS device"
 fi
